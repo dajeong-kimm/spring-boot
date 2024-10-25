dda377488de9efdca562b4513ee17527742219b9
김다정 <rlaekwjd6566@gmail.com>
2024-10-25 17:39:53 +0900

    Entity 작성중 백업

---
+++ demo/src/main/java/com/example/demo/Question.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(length = 200)
+	private String subject;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
+
+}
